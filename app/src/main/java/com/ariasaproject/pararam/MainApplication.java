46c38a71a36a6b05095a94d7b7717cba49e7731b
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2026-03-26 20:49:14 +0800

    build release setup

--- app/src/main/java/com/ariasaproject/pararam/MainApplication.java
+++ app/src/main/java/com/ariasaproject/pararam/MainApplication.java
@@ -1,32 +1,13 @@
 package com.ariasaproject.pararam;
 
 import android.app.Application;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.Build;
-import android.util.Log;
 
-import java.io.File;
-import javax.inject.Inject;
-import java.lang.StringBuilder;
-import java.lang.String;
-
-public class MainApplication extends Application, Thread.UncaughtExceptionHandler {
+public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Thread.setDefaultUncaughtExceptionHandler(this)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            notificationManagerWrapper.createNotificationChannel(
-                channelId = AndroidNotificationManagerWrapper.CHANNEL_ID,
-                name = getString(R.string.app_name),
-                importance = NotificationManager.IMPORTANCE_DEFAULT,
-            );
-        }
     }
 
     @Override
@@ -39,5 +20,4 @@ public class MainApplication extends Application, Thread.UncaughtExceptionHandle
         super.onConfigurationChanged(newConfig);
     }
     
-    public void unchaught
 }
